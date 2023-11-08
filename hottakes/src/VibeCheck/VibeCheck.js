78b0fe1d795a5272c3b81ce34bc15ace9e665ed4
shrishti2150 <38713821+shrishti2150@users.noreply.github.com>
2023-11-08 01:37:36 -0500

    vibecheck screen

--- hottakes/src/VibeCheck/VibeCheck.js
+++ hottakes/src/VibeCheck/VibeCheck.js
@@ -2,16 +2,28 @@ import React from "react";
 import "./VibeCheck.css";
 import "bulma/css/bulma.min.css";
 import arrow from "../assets/arrow.png";
+import vibe from "../assets/vibe.png";
 
 const VibeCheck = () => {
   return (
-    <div className="vibe">
-      <div>There’s no dress code,</div>
-      <div className="columns">
-        <div className="column">but there is a</div>
-        <div className="column">vibe check</div>
-        <div>
-          <img src={arrow} alt=""></img>
+    <div className="container">
+      <div className="glow"></div>
+      <div className="vibe">
+        <div>There’s no dress code,</div>
+        <div className="columns">
+          <div>but there is a </div>
+          <div className=" grad"> &nbsp;vibe check</div>
+          <div>
+            <a href="https://apps.apple.com/us/app/hot-takes-social-map-reviews/id6446862951">
+              <img src={arrow} alt=""></img>
+            </a>
+          </div>
+        </div>
+        <div className="container">
+          <div className="box1"></div>
+          <div className="img">
+            <img src={vibe} alt=""></img>
+          </div>
         </div>
       </div>
     </div>
