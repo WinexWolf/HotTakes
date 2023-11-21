131ffa5af907e5dbe6086d6ee1b133c85493d08b
shrishti2150 <38713821+shrishti2150@users.noreply.github.com>
2023-11-21 02:02:34 -0500

    Added animation package

--- hottakes/src/body/body.js
+++ hottakes/src/body/body.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./body.css";
 import Footer from "../footer/footer";
+import { Animator, ScrollContainer, ScrollPage } from "react-scroll-motion";
 
 const Body = () => {
-  return <div className="body-div">{/* <Footer /> */}</div>;
+  return (
+    <ScrollContainer snap="mandatory">
+      <ScrollPage>
+        <Animator>
+          <h1>Let me show you scroll animation 😀</h1>
+        </Animator>
+      </ScrollPage>
+    </ScrollContainer>
+  );
 };
 export default Body;
