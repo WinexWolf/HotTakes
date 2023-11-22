4fddf030d457d65d0e0a5103ddb7975422f060b8
shrishti2150 <38713821+shrishti2150@users.noreply.github.com>
2023-11-22 01:55:48 -0500

    updated audio screen

--- hottakes/src/takes/takes.js
+++ hottakes/src/takes/takes.js
@@ -7,12 +7,17 @@ import andrewAudio from "../assets/andrew.mp3";
 import marieAudio from "../assets/marie.mp3";
 import yeunjaeAudio from "../assets/yeunjae.mp3";
 import { PiForkKnifeFill } from "react-icons/pi";
+import { LuFerrisWheel } from "react-icons/lu";
+import { BiSolidDrink } from "react-icons/bi";
+import { IoMdPlayCircle } from "react-icons/io";
 
 const Takes = () => {
   const [activeImage, setActiveImage] = useState(img1); // Initial image
+  const [activeButton, setActiveButton] = useState(null); // Track active button
+
   const audioRef = useRef(null); // Reference to the audio element
 
-  const handleAudioPlay = (image, audio) => {
+  const handleAudioPlay = (image, audio, buttonName) => {
     if (audioRef.current) {
       audioRef.current.pause(); // Pause currently playing audio
       audioRef.current.currentTime = 0; // Reset the audio to the beginning
@@ -21,34 +26,77 @@ const Takes = () => {
     setActiveImage(image); // Change the displayed image based on the button clicked
     audioRef.current = new Audio(audio);
     audioRef.current.play(); // Play the associated audio
+
+    setActiveButton(buttonName); // Set the active button
   };
 
   return (
-    <div className="container">
+    <div className="container pad">
       <div className="columns ">
-        <div className="column  ">
-          <h1 className="grad">Plus, it’s really fun. </h1>
-          <div className="rounded-box">
-            <PiForkKnifeFill />
-            <button onClick={() => handleAudioPlay(img1, andrewAudio)}>
-              Play Andrew
+        <div className="column">
+          <h1 className="grad ">Plus, it’s really fun. </h1>
+          <br />
+
+          <div
+            className={`button-item ${
+              activeButton === "Fini Pizza" ? "active" : ""
+            }`}
+          >
+            <div className="round">
+              <PiForkKnifeFill />
+            </div>
+            <div className="loc-style">Fini Pizza</div>
+
+            <button
+              onClick={() => handleAudioPlay(img1, andrewAudio, "Fini Pizza")}
+            >
+              Play
+              <IoMdPlayCircle />
             </button>
           </div>
+          <br />
 
-          <button onClick={() => handleAudioPlay(img2, marieAudio)}>
-            Play Marie
-          </button>
-          <button onClick={() => handleAudioPlay(img3, yeunjaeAudio)}>
-            Play Yeunjae
-          </button>
+          <div
+            className={`button-item ${
+              activeButton === "Grand Leaners" ? "active" : ""
+            }`}
+          >
+            <div className="round">
+              <LuFerrisWheel />
+            </div>
+            <div className="loc-style">Grand Leaners</div>
+
+            <button
+              onClick={() => handleAudioPlay(img2, marieAudio, "Grand Leaners")}
+            >
+              Play
+              <IoMdPlayCircle />
+            </button>
+          </div>
+          <br />
+
+          <div
+            className={`button-item ${
+              activeButton === "Hell Phone" ? "active" : ""
+            }`}
+          >
+            <div className="round">
+              <BiSolidDrink />
+            </div>
+            <div className="loc-style">Hell Phone</div>
+
+            <button
+              onClick={() => handleAudioPlay(img3, yeunjaeAudio, "Hell Phone")}
+            >
+              Play
+              <IoMdPlayCircle />
+            </button>
+          </div>
         </div>
         <div className="column fixed">
-          <img
-            src={activeImage}
-            width="316"
-            height="600"
-            alt="Displayed Image"
-          />
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img src={activeImage} alt="Displayed Image" />
+          </div>
         </div>
       </div>
     </div>
