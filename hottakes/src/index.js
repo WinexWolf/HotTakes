9e61140ea126584cc56d56cfedc26333a310c563
shrishti2150 <38713821+shrishti2150@users.noreply.github.com>
2023-12-02 14:39:45 -0500

    added download url

--- hottakes/src/index.js
+++ hottakes/src/index.js
@@ -3,48 +3,43 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./Home/home";
 import Features from "./Features-main/features";
 import PlaceCurators from "./PlaceCurators/placeCurators";
 import About from "./About/about";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "home",
-    element: <Home />,
-  },
-  {
-    path: "community-guidelines",
-    element: <Features />,
-  },
-  {
-    path: "terms-and-conditions",
-    element: <PlaceCurators />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-]);
+const RedirectToExternalLink = () => {
+  const navigate = useNavigate();
 
-document.addEventListener("DOMContentLoaded", function () {
-  const rootElement = document.getElementById("root");
-  if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(
-      <React.StrictMode>
-        {/* <BrowserRouter> */}
-        <RouterProvider router={router} />
-        {/* </BrowserRouter> */}
-      </React.StrictMode>
-    );
-    reportWebVitals();
-  } else {
-    console.error("Root element not found!");
-  }
-});
+  // Redirect to the external link when this component is rendered
+  React.useEffect(() => {
+    window.location.href =
+      "https://apps.apple.com/us/app/hot-takes-social-map-reviews/id6446862951";
+  }, [navigate]);
+
+  return null; // To prevent rendering any content on this route
+};
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="home" element={<Home />} />
+        <Route path="community-guidelines" element={<Features />} />
+        <Route path="terms-and-conditions" element={<PlaceCurators />} />
+        <Route path="about" element={<About />} />
+        <Route path="download" element={<RedirectToExternalLink />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+reportWebVitals();
